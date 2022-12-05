89eeab0cb124da13f59f2fb2d7f2beffb5ab6f46
Andre Luiz <andre.lbressan.ayres@gmail.com>
2022-12-05 10:14:59 -0300

    aula 3

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,8 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse("contato")
-
-
-def sobre(request):
-    return HttpResponse("sobre")
+    return render(request, 'recipes/home.html', context={
+        'name': 'André Luiz',
+    })
